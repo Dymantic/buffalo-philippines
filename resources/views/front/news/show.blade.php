b4aa885cef470d6f06a2c196c8208d50a98c5c27
Michael Joyner <joyner.michael@gmail.com>
2017-12-08 12:24:31 +0800

    blog page stuff

--- resources/views/front/news/show.blade.php
+++ resources/views/front/news/show.blade.php
@@ -1,37 +1,41 @@
 @extends('front.base', ['pageName' => 'tool-bg'])
 
 @section('content')
-    <header class="banner-heading mb4 mt5 flex flex-column items-center justify-center tc">
-        <h1 class="ff-headline-small mb0">{{ $article->title }}</h1>
-        <p class="col-p ff-body">{{ $article->published_on->format('d F Y') }}</p>
-    </header>
-    <img src="{{ $article->titleImage() }}"
-         alt="Title image of {{ $article->title }}"
-         class="db mw7 mv5 center"
-    >
-    <section class="mw7 ff-body center-ns lh-copy mh3 article-body">
-        {!! $article->body !!}
-    </section>
-    <section class="tc mv5">
-        <p class="tc ff-title col-mg">Share this article</p>
-        <a href="#"
-           class="link col-d hv-col-p mh2">
-            @include('svgicons.facebook', ['classes' => 'icon ih5 hv-col-p'])
-        </a>
-        <a href="#"
-           class="link col-d hv-col-p mh2">
-            @include('svgicons.twitter', ['classes' => 'icon ih5'])
-        </a>
-        <a href="#"
-           class="link col-d hv-col-p mh2">
-            @include('svgicons.email', ['classes' => 'icon ih5'])
-        </a>
-    </section>
-
-    @include('front.partials.back-button', [
+    <div class="col-w-bg mw8 pv5 center-ns">
+        <header class="banner-heading mt0 flex flex-column items-center justify-center tc">
+            <h1 class="ff-headline-small mb0">{{ $article->title }}</h1>
+            <p class="col-p ff-body">{{ $article->published_on->format('d F Y') }}</p>
+        </header>
+        <img src="{{ $article->titleImage() }}"
+             alt="Title image of {{ $article->title }}"
+             class="db mw7 mv5 center"
+        >
+        <section class="mw7 ff-body center-ns lh-copy mh3 article-body">
+            {!! $article->body !!}
+        </section>
+        <section class="tc mv5">
+            <p class="tc ff-title col-mg">Share this article</p>
+            <a href="#"
+               class="link col-d hv-col-p mh2">
+                @include('svgicons.facebook', ['classes' => 'icon ih5 hv-col-p'])
+            </a>
+            <a href="#"
+               class="link col-d hv-col-p mh2">
+                @include('svgicons.twitter', ['classes' => 'icon ih5'])
+            </a>
+            <a href="#"
+               class="link col-d hv-col-p mh2">
+                @include('svgicons.email', ['classes' => 'icon ih5'])
+            </a>
+        </section>
+        @include('front.partials.back-button', [
             'link' => '/news',
             'block' => true,
             'classes' => 'mv5 col-p',
             'buttonText' => 'Back To Articles'
         ])
+    </div>
+
+
+
 @endsection
