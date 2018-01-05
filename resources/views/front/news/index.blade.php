9f57bda9da461b979852f866e485090854f08601
Michael Joyner <joyner.michael@gmail.com>
2018-01-05 08:38:14 +0800

    update laravel-mix and vue

--- resources/views/front/news/index.blade.php
+++ resources/views/front/news/index.blade.php
@@ -9,14 +9,14 @@
             <p class="measure-wide center-ns ff-large-body tc col-mg">There is currently no news to report. Be sure to check back in from time to time to stay up to date with all of our latest products and news.</p>
         @endif
         @foreach($articles as $article)
-            <div class="col-w-bg shadow-4 mb4 flex justify-between pt3">
+            <div class="col-w-bg shadow-4 mb4 flex justify-between">
                 <div class="dn w-25 flex-ns justify-center items-center">
                     <a href="/news/{{ $article->slug }}" class="link nospace">
                         <img src="{{ $article->titleImage('thumb') }}"
                              alt="Thumbnail of article's title image.">
                     </a>
                 </div>
-                <div class="pa2 pl4 flex-auto">
+                <div class="pr2 pb2 pl4 pt3 flex-auto">
                     <p class="ff-title b mv0"><a class="link col-d" href="/news/{{ $article->slug }}">{{ $article->title }}</a></p>
                     <p class="col-p ff-fine-body mv1">{{ $article->published_on->toFormattedDateString() }}</p>
                     <p class="measure-wide lh-title ff-fine-body">{{ $article->intro }}</p>
