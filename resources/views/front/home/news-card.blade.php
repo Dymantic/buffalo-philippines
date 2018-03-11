cd4cb5b220d5c9f2af220b7434da6e63af9bfa7b
Michael Joyner <joyner.michael@gmail.com>
2018-03-11 13:58:26 +0800

    simplify news section on home page

--- resources/views/front/home/news-card.blade.php
+++ resources/views/front/home/news-card.blade.php
@@ -1,21 +1,11 @@
-<div class="flex ma3 col-w-bg pa2">
-    <div class="w-20 dn db-ns">
-        <a href="/news/{{ $article->slug }}" class="">
-            <img src="{{ $article->titleImage('thumb') }}"
-                 alt="Title image for {{ $article->title }}" class="mw-100">
-        </a>
-
-    </div>
-    <div class="ph2 pt3 ph5-ns flex-auto">
-        <a href="/news/{{ $article->slug }}" class="link ff-title mb1 col-d hv-col-p">
+<div class="flex items-center ma3 col-w-bg pa2">
+        <p class="col-d ff-fine-body col-mg mr4">{{ $article->published_on->toFormattedDateString() }}</p>
+        <a href="/news/{{ $article->slug }}" class="link ff-title col-d hv-col-p flex-auto">
             {{ $article->title }}
         </a>
-        <p class="col-d mv1 ff-fine-body col-mg">{{ $article->published_on->toFormattedDateString() }}</p>
-        <p class="col-d measure-wide mt4 mt1-ns mb1 ff-fine-body">{{ $article->intro }}</p>
-        <p class="tr ma0 mt3 w-100">
+        <p class="ma0 mr2">
             <a href="/news/{{ $article->slug }}" class="link col-d hv-col-p">
                 @include('svgicons.button-arrow', ['classes' => 'icon-d ih3'])
             </a>
         </p>
-    </div>
 </div>
