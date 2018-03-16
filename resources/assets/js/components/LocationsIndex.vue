7e519209abff06c9d3b8d3c860efeb40348cb6ad
Michael Joyner <joyner.michael@gmail.com>
2018-03-16 10:16:07 +0800

    can search stores in admin and show total count of stores

--- resources/assets/js/components/LocationsIndex.vue
+++ resources/assets/js/components/LocationsIndex.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <location-item v-for="location in locations"
+        <p class="f4 ttu col-p">{{ locations.length }} Store Locations</p>
+        <div class="card mv3 flex justify-between items-center">
+            <p>Search location by name or address:</p>
+                <input type="text" v-model="query" class="w5 db h2">
+        </div>
+        <location-item v-for="location in matched_locations"
                        :key="location.id"
                        @remove-location="removeLocation"
                        :location-attributes="location"
@@ -14,14 +19,29 @@
 
         data() {
           return {
-              locations: this.list || []
+              locations: this.list || [],
+              query: ''
           };
+        },
+
+        computed: {
+            matched_locations() {
+                if(! this.query) {
+                    return this.locations;
+                }
+
+                return this.locations.filter(location => this.matchesQuery(location));
+            }
         },
 
         methods: {
             removeLocation(id) {
                 const index = this.locations.indexOf(this.locations.find(l => l.id === id));
                 this.locations.splice(index, 1);
+            },
+
+            matchesQuery(location) {
+                return (location.name.toLowerCase().indexOf(this.query.toLocaleLowerCase()) !== -1) || (location.address.toLowerCase().indexOf(this.query.toLowerCase()) !== -1);
             }
         }
     }
