71489468b16c16a09bd2642834dffab76fbb78e2
Michael Joyner <joyner.michael@gmail.com>
2018-03-19 11:17:41 +0800

    include product counts in search page

--- app/Http/Controllers/Admin/ProductSearchController.php
+++ app/Http/Controllers/Admin/ProductSearchController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Products\Category;
+use App\Products\Product;
 use App\Products\ProductsRepository;
+use App\Products\Subcategory;
+use App\Products\ToolGroup;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,7 +14,22 @@ class ProductSearchController extends Controller
 {
     public function index(ProductsRepository $repository)
     {
-        $products = request('q') ? $repository->fullSearch(request('q'))->map->toJsonableArray() : [];
-        return view('admin.products.search.index', ['products' => $products, 'search_term' => request('q', '')]);
+        $product_count = Product::count();
+        $category_count = Category::count();
+        $subcategory_count = Subcategory::count();
+        $toolgroup_count = ToolGroup::count();
+
+        $products = request('q')
+            ? $repository->fullSearch(request('q'))->map->toJsonableArray()
+            : [];
+
+        return view('admin.products.search.index', [
+            'products'      => $products,
+            'search_term'   => request('q', ''),
+            'product_count' => $product_count,
+            'category_count' => $category_count,
+            'subcategory_count' => $subcategory_count,
+            'toolgroup_count' => $toolgroup_count,
+        ]);
     }
 }
--- resources/views/admin/products/search/index.blade.php
+++ resources/views/admin/products/search/index.blade.php
@@ -8,6 +8,9 @@
                class="btn">See Categories</a>
         </div>
     </div>
+    <div class="mv3">
+        <p class="ff-large-body">Search <span class="ff-headline-small col-p">{{ $product_count }}</span> products, in <span class="ff-headline-small col-p">{{ $category_count }}</span> categories, <span class="ff-headline-small col-p">{{ $subcategory_count }}</span> subcategories and <span class="ff-headline-small col-p">{{ $toolgroup_count }}</span> tool groups.</p>
+    </div>
     <product-search search-url="/admin/services/search/products"
                     :initial-results='{{ json_encode($products) }}'
                     initial-term="{{ $search_term }}"
