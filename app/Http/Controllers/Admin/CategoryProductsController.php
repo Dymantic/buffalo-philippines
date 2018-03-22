bcdb87c2f0cb2e32a00381fa35eff8b19bcc07b8
Michael Joyner <joyner.michael@gmail.com>
2018-03-22 12:01:56 +0800

    clean up admin CategoryProductsController, remove unnecessary private method

--- app/Http/Controllers/Admin/CategoryProductsController.php
+++ app/Http/Controllers/Admin/CategoryProductsController.php
@@ -18,58 +18,9 @@ class CategoryProductsController extends Controller
 
     public function index(Category $category)
     {
-
-        $viewOptions = $this->viewOptions();
-
-
         return view('admin.categories.products.index', [
             'category'        => $category,
             'categoryMenu'    => $category->menu(),
-            'showType'        => $viewOptions['type'],
-            'toolGroup'       => $viewOptions['tool_group'],
-            'subcategory'     => $viewOptions['sub'],
-            'menuSubcategory' => $viewOptions['menuSubcategory']
         ]);
-    }
-
-    private function viewOptions()
-    {
-        $subcategory = Subcategory::find(request('subcategory'));
-        $toolGroup = ToolGroup::find(request('tool-group'));
-
-        if (!$toolGroup && !$subcategory) {
-            return [
-                'sub'             => ['id' => null, 'title' => '', 'tool_groups' => []],
-                'type'            => 'Category',
-                'tool_group'      => ['id' => null, 'title' => ''],
-                'menuSubcategory' => null
-            ];
-        }
-
-        if ($subcategory) {
-            return [
-                'sub'             => [
-                    'id'          => $subcategory->id,
-                    'title'       => $subcategory->title,
-                    'tool_groups' => $subcategory->toolGroups->pluck('id')->all()
-                ],
-                'type'            => 'Subcategory',
-                'tool_group'      => ['id' => null, 'title' => ''],
-                'menuSubcategory' => $subcategory->id
-            ];
-        }
-
-        $sub = $toolGroup->subcategory;
-
-        return [
-            'sub'             => [
-                'id'          => $sub->id,
-                'title'       => $sub->title,
-                'tool_groups' => $sub->toolGroups->pluck('id')->all()
-            ],
-            'type'            => 'Tool Group',
-            'tool_group'      => ['id' => $toolGroup->id, 'title' => $toolGroup->title],
-            'menuSubcategory' => $sub->id
-        ];
     }
 }
