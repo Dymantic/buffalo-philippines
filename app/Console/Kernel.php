5b4a10d673a04c184ed528f2d01f4615e98eaaa4
Michael Joyner <joyner.michael@gmail.com>
2018-07-03 20:35:20 +0800

    add sitemap generation to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('backup:run')->dailyAt('12:00');
         $schedule->command('backup:clean')->dailyAt('12:30');
+        $schedule->command('sitemap:generate')->dailyAt('13:00');
     }
 
     /**
